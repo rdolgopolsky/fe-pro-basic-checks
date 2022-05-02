53dbc3e25b085aab0fce66cc1b8301172c5d25c5
rdolgopolsky <rdolgopolsky@gmail.com>
2022-05-02 23:02:36 +0300

    homework done

--- src/checks.js
+++ src/checks.js
@@ -5,6 +5,7 @@ import {
   consoleLoggerWordsForOf,
   consoleLoggerWordsFor,
   reducerIf,
+  reducerSwitch,
   capitalizeString,
 } from './index.js';
 
@@ -39,3 +40,9 @@ console.log(reducerIf('lowercase', 'STRing')); // string
 console.log(reducerIf('fence', 'string')); // sTrInG
 console.log(reducerIf('capitalize', 'test test')); // Test Test
 console.log(reducerIf('bla', 'string')); // string
+
+console.log(reducerSwitch('uppercase', 'string')); // STRING
+console.log(reducerSwitch('lowercase', 'STRing')); // string
+console.log(reducerSwitch('fence', 'string')); // sTrInG
+console.log(reducerSwitch('capitalize', 'test test')); // Test Test
+console.log(reducerSwitch('bla', 'string')); // string
--- src/index.js
+++ src/index.js
@@ -3,14 +3,29 @@
  * @param {string} string
  * @returns {string}
  */
-export const capitalizeString = 0;
+
+export const capitalizeString = (string) => string.split(" ").map((word) => `${word.charAt(0).toUpperCase()}${word.slice(1)}`).join(" ");;
 
 /**
  * Должна быть function declaration
  * @param {string} string
  * @returns {string}
  */
-export const fenceString = 0;
+
+export function fenceString(string) {
+  let str = string.split("");
+  let newStr = [];
+
+  for (let i = 0; i < str.length; i++) {
+    if (i % 2) {
+      newStr.push(str[i].toUpperCase());
+    } else {
+      newStr.push(str[i].toLowerCase());
+    }
+  }
+
+  return newStr.join("");
+};
 
 /**
  * Должна быть function expression
@@ -18,7 +33,24 @@ export const fenceString = 0;
  * @param {string} string
  * @returns {string}
  */
-export const reducerIf = 0;
+
+export const reducerIf = function (action, string) {
+  if (action === "uppercase") {
+    return string.toUpperCase();
+  }
+  else if (action === "lowercase") {
+    return string.toLowerCase();
+  }
+  else if (action === "capitalize") {
+    return capitalizeString(string);
+  }
+  else if (action === "fence") {
+    return fenceString(string);
+  }
+  else {
+    return string;
+  }
+};
 
 /**
  * Стрелочная
@@ -26,28 +58,62 @@ export const reducerIf = 0;
  * @param {string} string
  * @returns {string}
  */
-export const reducerSwitch = 0;
+
+export const reducerSwitch = (action, string) => {
+  switch (action) {
+    case "uppercase":
+      return string.toUpperCase();
+    case "lowercase":
+      return string.toLowerCase();
+    case "capitalize":
+      return capitalizeString(string);
+    case "fence":
+      return fenceString(string);
+    default:
+      return string;
+  }
+};
 
 /**
  Стрелочная
  * @param {string} string
  */
-export const consoleLoggerWordsForOf = 0;
+
+export const consoleLoggerWordsForOf = (string) => {
+  for (let letter of string) {
+    console.log(letter);
+  }
+};
 
 /**
  Стрелочная
  * @param {string} string
  */
-export const consoleLoggerWordsFor = 0;
+
+export const consoleLoggerWordsFor = (string) => {
+  for (let i = 0; i < string.length; i++) {
+    console.log(string[i]);
+  }
+};
 
 /**
  Стрелочная
  * @param {string} string
  */
-export const consoleLoggerWordsWhile = 0;
+
+export const consoleLoggerWordsWhile = (string) => {
+  let i = 0;
+  do {
+    console.log(string[i]);
+    i++;
+  }
+  while (i < string.length)
+};
 
 /**
  Стрелочная
  * @param {string} string
  */
-export const consoleLoggerWordsSplit = 0;
+
+export const consoleLoggerWordsSplit = (string) => string.split("").forEach(item => console.log(item));
+
